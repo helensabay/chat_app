915f55424faeb5231679f2fb1dd6de61e63813ee
helensabay <helensabay14@gmail.com>
2025-05-27 20:17:55 +0800

    Building Basic Widgets for the login screen

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -4,13 +4,14 @@ class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print('Button clicked');
+          print('Button clicked!');
         },
       ),
       body: Column(
