f7a7a1a60314a11612f287dd825e75ff79550509
helensabay <helensabay14@gmail.com>
2025-05-27 21:01:26 +0800

    Building Basic widgets for the chat screen

---
+++ lib/chat_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text('Hi Helen!'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                print('Icon pressed!');
+              },
+              icon: Icon(Icons.logout))
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Hi, this is your message!',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                Image.network(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRckL6ah3SCkL9b-rR8TBiChatiyVzsWRVmXA&s',
+                    height: 200),
+              ],
+            ),
+            margin: EdgeInsets.all(50),
+            decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                    bottomLeft: Radius.circular(12))),
+          ),
+          Container(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Hi, this is your message!',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                Image.network(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRckL6ah3SCkL9b-rR8TBiChatiyVzsWRVmXA&s',
+                    height: 200),
+              ],
+            ),
+            margin: EdgeInsets.all(50),
+            decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                    bottomLeft: Radius.circular(12))),
+          ),
+          Container(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Hi, this is your message!',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                Image.network(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRckL6ah3SCkL9b-rR8TBiChatiyVzsWRVmXA&s',
+                    height: 200),
+              ],
+            ),
+            margin: EdgeInsets.all(50),
+            decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                    bottomLeft: Radius.circular(12))),
+          ),
+          Container(
+            height: 100,
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+          )
+        ],
+      ),
+    );
+  }
+}
