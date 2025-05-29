eaed72b6f338e00b8ce16d390dbf7da17e8d5500
helensabay <helensabay14@gmail.com>
2025-05-30 02:39:44 +0800

    Generics in Dart

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -4,7 +4,28 @@ import 'package:chat_app/chat_input.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  ChatPage({Key? key}) : super(key: key);
+
+  List<ChatMessageEntity> _messages = [
+    ChatMessageEntity(
+        author: Author(userName: 'pooja'),
+        createdAt: 2131231242,
+        id: '1',
+        text: 'First text'),
+    ChatMessageEntity(
+        author: Author(userName: 'pooja'),
+        createdAt: 2131231442,
+        id: '1',
+        text: 'Second text',
+        imageUrl:
+            'https://images.unsplash.com/photo-1605749439419-80c81f67eefc?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvd2VycyUyMGFlc3RoZXRpY3xlbnwwfHwwfHx8MA%3D%3D'),
+    ChatMessageEntity(
+      author: Author(userName: 'jane'),
+      createdAt: 2131234242,
+      id: '1',
+      text: 'Third text',
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,17 +50,13 @@ class ChatPage extends StatelessWidget {
         children: [
           Expanded(
             child: ListView.builder(
-                itemCount: 10,
+                itemCount: _messages.length,
                 itemBuilder: (context, index) {
                   return ChatBubble(
-                      alignment: index % 2 == 0
-                          ? Alignment.centerLeft
-                          : Alignment.centerRight,
-                      entity: ChatMessageEntity(
-                          id: '1234',
-                          text: 'Hello this is Helen!!!',
-                          createdAt: DateTime.now().millisecondsSinceEpoch,
-                          author: Author(userName: 'helensabay')));
+                      alignment: _messages[index].author.userName == 'pooja'
+                          ? Alignment.centerRight
+                          : Alignment.centerLeft,
+                      entity: _messages[index]);
                 }),
           ),
           ChatInput(),
--- lib/counter_stateful_demo.dart
+++ lib/counter_stateful_demo.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CounterStateful extends StatefulWidget {
-  Color buttonColor;
-
+  final Color buttonColor;
   CounterStateful({Key? key, required this.buttonColor}) : super(key: key);
 
   @override
@@ -36,22 +35,21 @@ class _CounterStatefulState extends State<CounterStateful> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter!!!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: widget.buttonColor,
-        child: Icon(Icons.message),
-        onPressed: () {
-          increment();
-        },
-      ),
-      body: Center(
-        child: Text(
-          '$counter',
-          style: TextStyle(fontSize: 30),
+        appBar: AppBar(
+          title: Text('Counter'),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: widget.buttonColor,
+          child: Icon(Icons.add),
+          onPressed: () {
+            increment();
+          },
+        ),
+        body: Center(
+          child: Text(
+            '$counter',
+            style: TextStyle(fontSize: 30),
+          ),
+        ));
   }
 }
