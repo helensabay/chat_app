7e3a7e11c20b7e7c1a9ae9226c5cea8136526eb4
helensabay <helensabay14@gmail.com>
2025-05-29 17:47:30 +0800

    Validating User Inputs

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,11 +3,16 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
 
-  void loginUser() {
-    print(userNameController.text);
-    print(passwordController.text);
+  final _formkey = GlobalKey<FormState>();
 
-    print('Login successful!');
+  void loginUser() {
+    if (_formkey.currentState != null && _formkey.currentState!.validate()) {
+      print(userNameController.text);
+      print(passwordController.text);
+      print('login! successful!');
+    } else {
+      print('not successful');
+    }
   }
 
   final userNameController = TextEditingController();
@@ -44,27 +49,47 @@ class LoginPage extends StatelessWidget {
                 'https://images.unsplash.com/photo-1605749439419-80c81f67eefc?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvd2VycyUyMGFlc3RoZXRpY3xlbnwwfHwwfHx8MA%3D%3D',
                 height: 200,
               ),
-              TextField(
-                controller: userNameController,
-                decoration: InputDecoration(
-                    hintText: 'Add your username',
-                    hintStyle: TextStyle(color: Colors.blueGrey),
-                    border: OutlineInputBorder()),
-              ),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                    hintText: 'Type your password',
-                    hintStyle: TextStyle(color: Colors.blueGrey),
-                    border: OutlineInputBorder()),
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value != null &&
+                            value.isNotEmpty &&
+                            value.length < 5) {
+                          return "Your username should be more than 5 characters";
+                        } else if (value != null && value.isEmpty) {
+                          return "Please type your username";
+                        }
+                        return null;
+                      },
+                      controller: userNameController,
+                      decoration: InputDecoration(
+                          hintText: 'Add your username',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: 'Type your password',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder()),
+                    ),
+                  ],
+                ),
               ),
               ElevatedButton(
-                  onPressed: loginUser,
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
-                  )),
+                onPressed: loginUser,
+                child: Text('Login',
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.w300)),
+              ),
               InkWell(
                 splashColor: Colors.red,
                 onDoubleTap: () {
@@ -74,13 +99,12 @@ class LoginPage extends StatelessWidget {
                   print('onLongpress!');
                 },
                 onTap: () {
-                  //TODO: Navigate to browser
                   print('Link clicked!');
                 },
                 child: Column(
                   children: [
                     Text('Find us on'),
-                    Text('https://helebsabay.com'),
+                    Text('https://helensabay.com'),
                   ],
                 ),
               )
