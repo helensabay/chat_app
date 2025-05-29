4db2becccfbb34f198e40b8fd5e358bfda93a6df
helensabay <helensabay14@gmail.com>
2025-05-29 16:25:24 +0800

    When to use stateless or stateful

---
+++ lib/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewWidget extends StatefulWidget {
+  const NewWidget({Key? key}) : super(key: key);
+
+  @override
+  State<NewWidget> createState() => _NewWidgetState();
+}
+
+class _NewWidgetState extends State<NewWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
