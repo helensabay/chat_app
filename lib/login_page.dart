963784516241120d0e01336d931b9e262e9c252d
helensabay <helensabay14@gmail.com>
2025-05-30 02:29:03 +0800

    Adding packages from external sources

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,28 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:chat_app/widgets/login_text_field.dart';
+// import 'dart:js';
+
+import 'package:chat_app/chat_page.dart';
 import 'package:chat_app/utils/spaces.dart';
+import 'package:chat_app/utils/textfield_styles.dart';
+import 'package:chat_app/widgets/login_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:social_media_buttons/social_media_buttons.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
 
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController userNameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final String _mainUrl = "https://poojabhaumik.com";
+  final _formkey = GlobalKey<FormState>();
 
-  void loginUser(BuildContext context) {
-    if (_formKey.currentState != null && _formKey.currentState!.validate()) {
+  void loginUser(context) {
+    if (_formkey.currentState != null && _formkey.currentState!.validate()) {
       print(userNameController.text);
       print(passwordController.text);
 
       Navigator.pushReplacementNamed(context, '/chat',
           arguments: '${userNameController.text}');
-      print('Login successful!');
+      print('login! successful!');
     } else {
-      print('Login failed!');
+      print('not successful');
     }
   }
+
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  final _mainUrl = "https://poojabhaumik.com";
 
   @override
   Widget build(BuildContext context) {
@@ -30,94 +37,96 @@ class LoginPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  'Let\'s sign you in!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Let\'s sign you in!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
                     fontSize: 30,
                     color: Colors.black,
                     fontWeight: FontWeight.bold,
-                  ),
-                ),
-                verticalSpacing(16),
-                Text(
-                  'Welcome back! \nYou\'ve been missed!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
+                    letterSpacing: 0.5),
+              ),
+              Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
                     fontSize: 20,
-                    color: Colors.blueGrey,
-                  ),
+                    color: Colors.blueGrey),
+              ),
+              Image.asset(
+                'assets/illustration.png',
+                height: 200,
+              ),
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    LoginTextField(
+                      hintText: "Enter your username",
+                      validator: (value) {
+                        if (value != null &&
+                            value.isNotEmpty &&
+                            value.length < 5) {
+                          return "Your username should be more than 5 characters";
+                        } else if (value != null && value.isEmpty) {
+                          return "Please type your username";
+                        }
+                        return null;
+                      },
+                      controller: userNameController,
+                    ),
+                    verticalSpacing(24),
+                    LoginTextField(
+                      hasAsterisks: true,
+                      controller: passwordController,
+                      hintText: 'Enter your password',
+                    ),
+                  ],
                 ),
-                verticalSpacing(24),
-                Image.asset(
-                  'assets/illustration.png',
-                  height: 200,
+              ),
+              verticalSpacing(24),
+              ElevatedButton(
+                onPressed: () {
+                  loginUser(context);
+                },
+                child: Text('Login',
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.w300)),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  print('Link Clicked!');
+                  final Uri url = Uri.parse(_mainUrl);
+                  if (!await launchUrl(url)) {
+                    throw 'Could not launch this!';
+                  }
+                },
+                child: Column(
+                  children: [
+                    Text('Find us on'),
+                    Text(_mainUrl),
+                  ],
                 ),
-                verticalSpacing(24),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      LoginTextField(
-                        hintText: "Enter your username",
-                        controller: userNameController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return "Please type your username";
-                          } else if (value.length < 5) {
-                            return "Username must be at least 5 characters";
-                          }
-                          return null;
-                        },
-                      ),
-                      verticalSpacing(16),
-                      LoginTextField(
-                        hintText: "Enter your password",
-                        controller: passwordController,
-                        hasAsterisks: true,
-                      ),
-                    ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialMediaButton.facebook(
+                    size: 20,
+                    color: Colors.blue,
+                    url: "https://www.facebook.com/share/1BPmT72qMA/",
                   ),
-                ),
-                verticalSpacing(24),
-                ElevatedButton(
-                  onPressed: () {
-                    loginUser(context);
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-                verticalSpacing(16),
-                InkWell(
-                  onTap: () async {
-                    final Uri url = Uri.parse(_mainUrl);
-                    if (!await launchUrl(url,
-                        mode: LaunchMode.externalApplication)) {
-                      throw 'Could not launch $_mainUrl';
-                    }
-                  },
-                  child: Column(
-                    children: [
-                      Text('Find us on'),
-                      Text(
-                        _mainUrl,
-                        style: TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+                  SocialMediaButton.linkedin(
+                    url: "https://www.linkedin.com/in/alvic-caranzo-b724a4170/",
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
