1384d9729f5692be2a33e07471905b05c4c801a9
helensabay <helensabay14@gmail.com>
2025-05-29 17:17:55 +0800

    Learning About Buttons

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,34 +3,54 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  void loginUser() {
+    print('Login successful!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Let\'s sign you in!',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5),
-            ),
-            Text(
-              'Welcome back! \n You\'ve been missed!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.blueGrey),
-            ),
-            Image.network(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHESCKUpFVDDEsbfCkqebFqjWiQ0lHpkUCpw&s',
-                height: 200),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Let\'s sign you in!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5),
+              ),
+              Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.blueGrey),
+              ),
+              Image.network(
+                'https://images.unsplash.com/photo-1605749439419-80c81f67eefc?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvd2VycyUyMGFlc3RoZXRpY3xlbnwwfHwwfHx8MA%3D%3D',
+                height: 200,
+              ),
+              ElevatedButton(
+                  onPressed: loginUser,
+                  child: Text(
+                    'Click me!',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+                  )),
+              TextButton(
+                  onPressed: () {
+                    print('Pressed on the URL!');
+                  },
+                  child: Text('https://helensabay.com'))
+            ],
+          ),
         ),
       ),
     );
