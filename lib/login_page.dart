12db8341df7cc49ad60846cd678d66f138523447
helensabay <helensabay14@gmail.com>
2025-05-29 17:37:51 +0800

    Getting Inputs from the User

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
 
   void loginUser() {
+    print(userNameController.text);
+    print(passwordController.text);
+
     print('Login successful!');
   }
+
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -38,11 +44,26 @@ class LoginPage extends StatelessWidget {
                 'https://images.unsplash.com/photo-1605749439419-80c81f67eefc?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvd2VycyUyMGFlc3RoZXRpY3xlbnwwfHwwfHx8MA%3D%3D',
                 height: 200,
               ),
+              TextField(
+                controller: userNameController,
+                decoration: InputDecoration(
+                    hintText: 'Add your username',
+                    hintStyle: TextStyle(color: Colors.blueGrey),
+                    border: OutlineInputBorder()),
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: 'Type your password',
+                    hintStyle: TextStyle(color: Colors.blueGrey),
+                    border: OutlineInputBorder()),
+              ),
               ElevatedButton(
                   onPressed: loginUser,
                   child: Text(
-                    'Click me!',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+                    'Login',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
                   )),
               InkWell(
                 splashColor: Colors.red,
@@ -59,7 +80,7 @@ class LoginPage extends StatelessWidget {
                 child: Column(
                   children: [
                     Text('Find us on'),
-                    Text('https://helensabay.com'),
+                    Text('https://helebsabay.com'),
                   ],
                 ),
               )
