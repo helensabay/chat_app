0e375d05743ea88f17dfc549d10f2da629f3868a
helensabay <helensabay14@gmail.com>
2025-05-30 08:07:07 +0800

    Exception handling

--- lib/widgets/picker_body.dart
+++ lib/widgets/picker_body.dart
@@ -18,15 +18,16 @@ class NetworkImagePickerBody extends StatelessWidget {
         future: _imageRepo.getNetworkImages(),
         builder: (BuildContext context,
             AsyncSnapshot<List<PixelfordImage>> snapshot) {
-          if (snapshot.hasData)
+          if (snapshot.hasData) {
             return GridView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 return GestureDetector(
-                    onTap: () {
-                      onImageSelected(snapshot.data![index].urlFullSize);
-                    },
-                    child: Image.network(snapshot.data![index].urlFullSize));
+                  onTap: () {
+                    onImageSelected(snapshot.data![index].urlFullSize);
+                  },
+                  child: Image.network(snapshot.data![index].urlFullSize),
+                );
               },
               gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                 crossAxisSpacing: 2,
@@ -34,7 +35,12 @@ class NetworkImagePickerBody extends StatelessWidget {
                 maxCrossAxisExtent: MediaQuery.of(context).size.width * 0.5,
               ),
             );
-          //return Image.network(snapshot.data![0].urlFullSize);
+          } else if (snapshot.hasError) {
+            return Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Text('This is the error: ${snapshot.error}'),
+            );
+          }
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Center(child: CircularProgressIndicator()),
