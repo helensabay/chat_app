431af34adf709409f157f991a01f2f71245abba5
helensabay <helensabay14@gmail.com>
2025-05-30 08:41:50 +0800

    Change notifiers in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(Provider(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AuthService.init();
+  runApp(ChangeNotifierProvider(
     create: (BuildContext context) => AuthService(),
     child: ChatApp(),
   ));
